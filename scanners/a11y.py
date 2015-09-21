4f4b5563fbb73fb31baa8e56ba65d25c7d51c2e7
Eric Mill <eric.mill@gsa.gov>
2015-09-21 17:15:14 -0400

    fix up scanning arguments for pa11y, implement caching

--- scanners/a11y.py
+++ scanners/a11y.py
@@ -2,22 +2,9 @@ import logging
 from scanners import utils
 import json
 import os
-
 import shlex
 from subprocess import Popen, PIPE
 import json
-
-def get_exitcode_stdout_stderr(cmd):
-    """
-    Execute the external command and get its exitcode, stdout and stderr.
-    """
-    args = shlex.split(cmd)
-
-    proc = Popen(args, stdout=PIPE, stderr=PIPE)
-    out, err = proc.communicate()
-    exitcode = proc.returncode
-
-    return exitcode, out, err
 
 
 command = os.environ.get("PA11Y_PATH", "pa11y")
@@ -34,14 +21,35 @@ headers = [
     "selector"
 ]
 
-def scan(domain, options):
-    print(options)
-    command = "pa11y --standard %s --reporter json %s" % (PA11Y_STANDARD, domain)
-    exitcode, out, err = get_exitcode_stdout_stderr(command)
-    
-    reports = json.loads(out.decode("utf-8"))
 
-    for report in reports:
+def scan(domain, options):
+    command = ["pa11y", "--standard", PA11Y_STANDARD, "--reporter", "json", "--level", "none", domain]
+    logging.debug("\t %s" % command)
+    
+    cache = utils.cache_path(domain, "a11y")
+
+    if (options.get("force", False) is False) and (os.path.exists(cache)):
+        logging.debug("\tCached.")
+        raw = open(cache).read()
+        data = json.loads(raw)
+        if data.get('invalid'):
+            return None
+
+    else:
+        raw = utils.scan(command)
+
+        # ?? invalid somehow
+        if raw is None:
+            utils.write(utils.invalid({}), cache)
+            return None
+
+        data = json.loads(raw)
+        # Turn it into a dict rather than an array.
+        # Using json_for also pretty-prints the JSON to be human-readable.
+        data = {'reports': data}
+        utils.write(utils.json_for(data), cache)
+
+    for report in data['reports']:
         result = []
         for header in headers:
             result.append(report.get(header))
